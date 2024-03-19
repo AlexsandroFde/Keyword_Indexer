9d9f45ca85b94a26c17b84e6eadcb1692433fc0b
AlexsandroFde <alexsandro.ferreira02@aluno.ifce.edu.br>
2024-03-18 22:56:09 -0300

    added remissive index struct

---
+++ include/lib.h
@@ -0,0 +1,16 @@
+#ifndef LIB_H
+#define LIB_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#define max_palavras 1000
+#define max_palavra_tam 50
+
+typedef struct { // Estrutura para armazenar as palavras e suas frequencias
+  char palavra[max_palavra_tam];
+  int linhas[max_palavras];
+  int count;
+} IndiceRemissivo;
+
+#endif
